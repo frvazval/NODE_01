883c0be1886458640295801f3a5f1a1ef046b526
Fvazquez <foapgenerico@365.stucom.com>
2025-06-11 19:32:09 +0200

    stucom

--- 07_http_2.js
+++ 07_http_2.js
@@ -9,21 +9,40 @@ console.log(process.env.PASSWORD);
 
 const PUERTO = process.env.PORT|| process.argv[2] || 8888;
 
-const style = 
+const style = `
+<style>
+h1 {
+color: red;
+}
+</style>`
 
 const server = http.createServer((req, res) => {
-    console.log(res.statusCode);
+    
     // console.log("Has hecho una petición desde Node");
     if (req.url == "/") {
         // console.log("Conectados a la raiz del server");
         res.writeHead(200,{"content-type": "text/html"});
+        console.log(res.statusCode);
+        res.write(style);
         res.write("<h1>Estamos en / desde Node</h1>");
         res.end();
+        console.log(res.statusCode);
+        return
     } else if (req.url == "/contacto") {
-        console.log("Conectados a /contacto");
+        // console.log("Conectados a /contacto");
+        res.writeHead(200,{"content-type": "text/html"});        
+        res.write(style);
+        res.write("<h1>Estamos en /contacto</h1>");
+        res.end();
+        
+        return
     } else {
-        // console.log("Ruta desconocida");
-        res.writeHead(404,{"content-type": "text/html"});
+        // console.log("Ruta desconocida");        
+        res.writeHead(200,{"content-type": "text/html"});
+        res.write(style);
+        res.write("<h1>Error 404</h1>");
+        res.end();        
+        return
     }
 })
 
