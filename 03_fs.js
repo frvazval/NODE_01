ddc1562eee20723abb5102ef3ebb0b1540ae9303
Fvazquez <foapgenerico@365.stucom.com>
2025-06-11 17:55:07 +0200

    stucom

--- 03_fs.js
+++ 03_fs.js
@@ -1,11 +1,30 @@
 const fs = require("node:fs");
-let texto = "En un lugar de la Mancha de cuyo nombre";
-let append = " no quiero acordarme\n";
-fs.writeFileSync("prueba.txt", texto, "utf-8");
+// let texto = "En un lugar de la Mancha de cuyo nombre";
+// let append = " no quiero acordarme\n";
+// fs.writeFileSync("prueba.txt", texto, "utf-8");
 
-fs.appendFileSync("prueba.txt", append, "utf-8");
-append = "\t\tCervantes\n";
-fs.appendFileSync("prueba.txt", append, "utf-8");
+// fs.appendFileSync("prueba.txt", append, "utf-8");
+// append = "\t\tCervantes\n";
+// fs.appendFileSync("prueba.txt", append, "utf-8");
 
-let prueba = fs.readFileSync("prueba.txt", "utf-8");
-console.log(prueba);
+// let prueba = fs.readFileSync("prueba.txt", "utf-8");
+// console.log(prueba);
+
+// Para borrar el contenido de prueba.txt
+// fs.truncateSync("prueba.txt");
+
+
+// Crear y borrar directorios
+
+// if (fs.existsSync("temp")) {
+//     fs.rmdirSync("temp");
+//     console.log("Directorio borrado");
+// }
+// fs.mkdirSync("temp");
+
+// Crear varios directorios a la vez
+
+fs.mkdirSync("temp/temp2", {recursive: true});
+fs.mkdirSync("temp/temp1", {recursive: true});
+
+
