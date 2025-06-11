075bdd79fb0c87113c28baec988310790662fee5
Fvazquez <foapgenerico@365.stucom.com>
2025-06-11 19:16:17 +0200

    stucom

--- 07_http_2.js
+++ 07_http_2.js
@@ -9,15 +9,21 @@ console.log(process.env.PASSWORD);
 
 const PUERTO = process.env.PORT|| process.argv[2] || 8888;
 
+const style = 
+
 const server = http.createServer((req, res) => {
     console.log(res.statusCode);
     // console.log("Has hecho una petición desde Node");
     if (req.url == "/") {
-        console.log("Conectados a la raiz del server");
+        // console.log("Conectados a la raiz del server");
+        res.writeHead(200,{"content-type": "text/html"});
+        res.write("<h1>Estamos en / desde Node</h1>");
+        res.end();
     } else if (req.url == "/contacto") {
         console.log("Conectados a /contacto");
     } else {
-        console.log("Ruta desconocida");
+        // console.log("Ruta desconocida");
+        res.writeHead(404,{"content-type": "text/html"});
     }
 })
 
