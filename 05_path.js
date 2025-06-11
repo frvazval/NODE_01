ff5537dc1f7ccb8d914366e2a9829899b65b2fb7
Fvazquez <foapgenerico@365.stucom.com>
2025-06-11 18:02:55 +0200

    stucom

--- 05_path.js
+++ 05_path.js
@@ -3,3 +3,12 @@ const path = require("node:path");
 // separador de carpetas del SO
 console.log(path.sep);
 
+// Crear ruta con varias carpetas y un fichero
+const rutaInventada = path.join("project", "public", "css", "styles.css");
+console.log(rutaInventada);
+
+// Obtener el nombre y extensión del fichero
+console.log(path.basename(rutaInventada));
+
+// Obtener la ruta de las carpetas
+console.log(path.dirname(rutaInventada));
