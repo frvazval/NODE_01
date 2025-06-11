8ef8b4d5ee067cadded37028c5706b65236a320c
Fvazquez <foapgenerico@365.stucom.com>
2025-06-11 15:42:32 +0200

    stucom

---
+++ 01_process.js
@@ -0,0 +1,8 @@
+// console.log(process.versions.node);
+// console.log(process.versions.v8);
+// console.log(process.versions);
+
+// console.log(process.argv);
+
+// node .\01_process.js Paco, aparecera en pantalla Hola Paco
+console.log(`Hola ${process.argv[2]}`);
