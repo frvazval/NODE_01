5053cf01a98a3b19f7abd8ccb94e4bffb7d7530f
Fvazquez <foapgenerico@365.stucom.com>
2025-06-11 17:01:42 +0200

    stucom

--- 03_fs.js
+++ 03_fs.js
@@ -1,1 +1,6 @@
 const fs = require("node:fs");
+let texto = "En un lugar de la Mancha de cuyo nombre";
+let append = " no quiero acordarme";
+fs.writeFileSync("prueba.txt", texto, "utf-8");
+
+fs.appendFileSync("prueba.txt", append, "utf-8");
