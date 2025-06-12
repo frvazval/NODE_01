7613c0558af035aef71f03b17adcd24d0c16f462
Fvazquez <foapgenerico@365.stucom.com>
2025-06-12 15:25:27 +0200

    stucom

---
+++ 09_pizzeria_1.js
@@ -0,0 +1,14 @@
+const pizzas = [
+    {"tipo": "Cuatro Quesos", "precio": 18.00},
+    {"tipo": "Margherita", "precio": 16.00},
+    {"tipo": "Napolitana", "precio": 17.25},
+    {"tipo": "Diavola", "precio": 16.50}
+]
+
+// Para ejecutar el programa:
+// node 09_pizzeria_1.js
+
+let menu = "Pizzeria Nodini\n";
+menu += "*".repeat(menu.length -1);
+
+console.log("\n\n", menu);
