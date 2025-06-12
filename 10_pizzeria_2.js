ef0eba0a379991ae4e0c135dbbe7e5ebfa2e4d28
Fvazquez <foapgenerico@365.stucom.com>
2025-06-12 16:01:37 +0200

    stucom

---
+++ 10_pizzeria_2.js
@@ -0,0 +1,9 @@
+const ingredientes = require("./ingredientes_pizza.js");
+
+console.log(ingredientes);
+
+// Paso 1 : Mostrar el menu de opciones al usuario
+
+let menu = `
+
+`;
---
+++ ingredientes_pizza.js
@@ -0,0 +1,16 @@
+const ingredientes = [
+    {"ingrediente": "masa normal", "precio": 5.00},
+    {"ingrediente": "masa de espelta", "precio": 6.00},
+    {"ingrediente": "tomate", "precio": 0.50},
+    {"ingrediente": "gorgonzola", "precio": 3.50},
+    {"ingrediente": "mozzarella", "precio": 2.50},
+    {"ingrediente": "prosciutto", "precio": 2.75},
+    {"ingrediente": "nduja", "precio": 3.75},
+    {"ingrediente": "parmigiano", "precio": 3.50},
+    {"ingrediente": "peperoni", "precio": 1.75},
+    {"ingrediente": "bacon", "precio": 2.00},
+    {"ingrediente": "funghi", "precio": 1.75},
+    {"ingrediente": "piña", "precio": 2.00}
+]
+
+module.exports = ingredientes
