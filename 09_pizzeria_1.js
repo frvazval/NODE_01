c61523e96ef730ecb59cae24dc7755de00eb232a
Fvazquez <foapgenerico@365.stucom.com>
2025-06-12 15:44:10 +0200

    stucom

--- 09_pizzeria_1.js
+++ 09_pizzeria_1.js
@@ -8,15 +8,27 @@ const pizzas = [
 // Para ejecutar el programa:
 // node 09_pizzeria_1.js
 
-let menu = "Pizzeria Nodini\n";
-menu += "*".repeat(menu.length -1);
+let mensaje = "";
+
+if (process.argv.length == 2) {
+    let menu = "Pizzeria Nodini\n";
+    menu += "*".repeat(menu.length -1);
 
 
 
-for (let i = 0; i < pizzas.length; i++ ) {
-    menu += `\n\t${i + 1}. Pizza ${pizzas[i].tipo} a ${pizzas[i].precio.toFixed(2)}€`
+    for (let i = 0; i < pizzas.length; i++ ) {
+        menu += `\n\t${i + 1}. Pizza ${pizzas[i].tipo} a ${pizzas[i].precio.toFixed(2)}€`
+    }
+
+    menu += "\n¿Cuál es su elección?\n\n";
+
+    console.log("\n\n", menu);
+} else if (process.argv.length == 3) {
+    let indice = process.argv[2];
+    mensaje = `
+    Has elegido ${pizzas[indice -1].tipo}, Importe : ${pizzas[indice - 1].precio.toFixed(2)}€
+    `;
+} else {
+    mensaje = "Demasiados argumentos en la petición"
 }
-
-menu += "\n¿Cuál es su elección?\n\n";
-
-console.log("\n\n", menu);
+console.log(mensaje, "\n\n");
